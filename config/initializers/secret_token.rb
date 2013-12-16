ad806138b8a80fe01c629cca48079e66d9d6752d
johndewolf <jdewolf06@gmail.com>
2013-12-16 08:58:20 -0500

    Added Blog DB Design

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BlogProject::Application.config.secret_key_base = 'cb639a297a63470d6b3aa6314f8ea310b7343df9ada25e71b8657703e5e94524ac075ecc513b842022767571483f841dcf34cf73f61d781592386b51d44c0ca5'
---
+++ db/migrate/20131214205747_adding_comments_id.rb
@@ -0,0 +1,5 @@
+class AddingCommentsId < ActiveRecord::Migration
+  def change
+    add_column :blogs, :comment_id, :integer
+  end
+end
